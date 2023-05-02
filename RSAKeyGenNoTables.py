b38bf36328d80791e1ad358d031a3ccc10a3af37
Art3mi0 <89485346+Art3mi0@users.noreply.github.com>
2023-05-02 12:07:26 -0400

    Tries changed to 1, checks if properly encrypts/decrypts

--- RSAKeyGenNoTables.py
+++ RSAKeyGenNoTables.py
@@ -7,6 +7,10 @@ Purpose - RSA Key Generation Project
 
 import random
 import secrets
+import time
+
+import FastExponentiationNoTables
+
 
 def euclidExtended(m, n):
     x = 0
@@ -65,7 +69,7 @@ def primalityTest(x, e, n):
 
 def RSAGen():
     goAgain = True
-    while goAgain:  # For the rare case not a single e has an inverse
+    while goAgain:  # For the extremely rare case not a single e has an inverse
         # Initializing Variables
         p = 0
         q = 0
@@ -74,12 +78,14 @@ def RSAGen():
         while q == 0:
             passedTest = False
             myNum = secrets.randbits(512)
+            myNum1 = myNum - 1
+            numTests = 1
 
             # Miller-Rabin Test begins
-            for i in range(20):
-                a = random.randint(2, myNum - 1)
-                result = primalityTest(a, myNum - 1, myNum)
-                if (result) and (i == 19):
+            for i in range(numTests):  # was 20
+                a = random.randint(2, myNum1)
+                result = primalityTest(a, myNum1, myNum)
+                if (result) and (i == numTests - 1):
                     passedTest = True
             if passedTest:
                 if p == 0:
@@ -105,21 +111,30 @@ def RSAGen():
                 goAgain = False
                 break
 
+    return n, e, d
     # Finalizing Results
-    print("p = %d, q = %d, n = %d, e = %d, d = %d" % (p, q, n, e, d))
-    binP = bin(p)[2:]
-    binQ = bin(q)[2:]
-    binN = bin(n)[2:]
-    binE = bin(e)[2:]
-    binD = bin(d)[2:]
+    # print("p = %d, q = %d, n = %d, e = %d, d = %d" % (p, q, n, e, d))
+    # binP = bin(p)[2:]
+    # binQ = bin(q)[2:]
+    # binN = bin(n)[2:]
+    # binE = bin(e)[2:]
+    # binD = bin(d)[2:]
+    #
+    # print("p = " + (32-len(binP))*"0" + binP)
+    # print("q = " + (32-len(binQ))*"0" + binQ)
+    # print("n = " + (32-len(binN))*"0" + binN)
+    # print("e = " + (32-len(binE))*"0" + binE)
+    # print("d = " + (32-len(binD))*"0" + binD)
 
-    print("p = " + (32-len(binP))*"0" + binP)
-    print("q = " + (32-len(binQ))*"0" + binQ)
-    print("n = " + (32-len(binN))*"0" + binN)
-    print("e = " + (32-len(binE))*"0" + binE)
-    print("d = " + (32-len(binD))*"0" + binD)
-
-RSAGen()
-#print(euclidExtended(3, 10600))
-num = secrets.randbits(512)
-print(num)
+for i in range(50):
+    sentence = "Attempt " + str(i + 1) + ": "
+    start = time.perf_counter()
+    n, e, d = RSAGen()
+    end = time.perf_counter()
+    print(sentence + str(end - start))
+    encrypt = FastExponentiationNoTables.expMod(69, e, n)
+    decrypt = FastExponentiationNoTables.expMod(encrypt, d, n)
+    if decrypt != 69:
+        print("Failed to properly decrypt \n")
+    else:
+        print("Properly decrypted \n")
